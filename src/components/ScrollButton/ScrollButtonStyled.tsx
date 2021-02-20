7b324c5164f36ff96e1ceea05c54aa21e87766f6
ErykSandecki <eryk.sandecki@gmail.com>
2021-02-20 23:48:21 +0100

    change styles of button scroll

--- src/components/ScrollButton/ScrollButtonStyled.tsx
+++ src/components/ScrollButton/ScrollButtonStyled.tsx
@@ -3,14 +3,15 @@ import styled from 'styled-components';
 
 export const ScrollButton = styled.div`
   align-items: center;
-  background: linear-gradient(to bottom, #82b632, #009add);
-  border-radius: 50%;
+  background-color: #646464;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
   bottom: 150px;
   display: flex;
   height: 40px;
   justify-content: center;
   position: fixed;
-  right: 10%;
+  right: 0;
   width: 40px;
   z-index: 10;
 `;
